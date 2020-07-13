e258ce5b7c6a2cc32a786818f0568bb1b3dcf9c4
Mikhail <naramanc@gmail.com>
2020-07-13 18:37:06 +0300

    task 1505

---
+++ 1505. Minimum Possible Integer After at Most K Adjacent Swaps On Digits/Task1505.java
@@ -0,0 +1,14 @@
+public class Task1505 {
+    public static void main(String[] args) {
+
+        System.out.println(new Solution().minInteger("4321", 4));
+
+    }
+
+}
+
+class Solution {
+    public String minInteger(String num, int k) {
+        return "";
+    }
+}
